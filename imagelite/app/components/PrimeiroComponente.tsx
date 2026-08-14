8dfaf61bc50d2efe17d5d7a6e6a0f85ea7b3fbd4
Ketlyn <souza.aguiar@estudante.ifmt.edu.br>
2026-08-14 13:06:54 -0400

    14/08/2026

--- imagelite/app/components/PrimeiroComponente.tsx
+++ imagelite/app/components/PrimeiroComponente.tsx
@@ -1,7 +1,26 @@
-export function PrimeiroComponente(){
-  return(
+'use client';
+
+interface PrimeiroComponenteProps {
+  mensagem: string;
+  mensagemBotao: string;
+}
+
+export const PrimeiroComponente = ({mensagem}: PrimeiroComponenteProps) => {
+  
+  //function clique(){
+  //  console.log("Você clicou no botão!");
+  //alert("Você clicou no botão!");
+  //}
+
+  const clique = () => {
+    console.log("Você clicou no botão!");
+    alert("Você cliclou no botão!");
+  }
+
+    return(
     <div> 
       <h1>Primeiro Componente</h1>
+      <button onClick={clique}>Clique aqui</button>
     </div>
   );
 } 
--- imagelite/app/formulario/page.tsx
+++ imagelite/app/formulario/page.tsx
@@ -1,9 +1,15 @@
+import { PrimeiroComponente } from "../components/PrimeiroComponente";
 
 export default function formularioPage() {
   return (
-    <main>
+   <div>
+     <main>
       <h1>Formulário</h1> 
-
+      <PrimeiroComponente
+      mensagem =  " Está é uma mensagem passada como propriedade do primeiroCompopnente" 
+      mensagemBotao = "Você clicou botão"
+      />
     </main>
+   </div>
   )
 } 
